23ede01e04c71131337d64c08d4ffea6ed2e879d
Emily Krohn <emilykrohn2@gmail.com>
2024-06-12 21:07:15 -0700

    Create Timer
    The timer switches back and forth from the amount of time for answering a question and the time for showing the answer to the question.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+
+    public bool isAnsweringQuestion = false;
+
+    float timerValue;
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (timerValue <= 0)
+        {
+            if (isAnsweringQuestion)
+            {
+                timerValue = timeToShowCorrectAnswer;
+                isAnsweringQuestion = false;
+            }
+            else
+            {
+                timerValue = timeToCompleteQuestion;
+                isAnsweringQuestion = true;
+            }
+        }
+
+        Debug.Log(timerValue);
+    }
+}
