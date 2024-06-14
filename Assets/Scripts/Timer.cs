084c5d73488b864c0466aff85c1846a17ff093ec
Emily Krohn <emilykrohn2@gmail.com>
2024-06-13 20:29:42 -0700

    Calculate Timer Fill Fraction
    It takes the current amount of time on the timer and divides it by the time to complete the question. UI images have a fill amount that ranges from 0 to 1 so the fraction calculated will be set to that fill amount and will get smaller over time.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,32 +7,50 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
-
+    public float fillFraction;
     float timerValue;
     void Update()
     {
         UpdateTimer();
     }
 
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+
     void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
-        if (timerValue <= 0)
+        if (isAnsweringQuestion)
         {
-            if (isAnsweringQuestion)
+            if (timerValue > 0)
             {
-                timerValue = timeToShowCorrectAnswer;
-                isAnsweringQuestion = false;
+                fillFraction = timerValue / timeToCompleteQuestion;
             }
             else
             {
-                timerValue = timeToCompleteQuestion;
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
+            {
                 isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
             }
         }
 
-        Debug.Log(timerValue);
+        Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
     }
 }
